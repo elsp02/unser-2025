f5bde77cdda80b006449aa4e5b77580a129b6552
elsp02 <elena.spitzer@hs-ausgburg.de>
2026-01-12 13:48:58 +0100

    layout bis Essen ohne Pano

---
+++ animations/scroll-animation.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+            const lenis = new Lenis();
+            lenis.on("scroll", ScrollTrigger.update);
+            gsap.ticker.add((time) => {
+                lenis.raf(time * 500);
+            });
+            gsap.ticker.lagSmoothing(0);
+
+            const scrollContainer = document.querySelector('.scroll-container');
+            const section = document.querySelector('.scroll-section');
+            const progressAmount = document.querySelector('.progress-amount');
+            
+
+            function initHorizontalScroll() {
+                
+                function getScrollAmount() {
+                    const scrollWidth = scrollContainer.scrollWidth;
+                    const windowWidth = window.innerWidth;
+                    
+                    return -(scrollWidth - windowWidth);
+                }
+
+                gsap.to(scrollContainer, {
+                    x: getScrollAmount,
+                    ease: "none",
+                    scrollTrigger: {
+                        trigger: section,
+                        pin: true,
+                        start: "top top",
+                        end: () => `+=${-getScrollAmount()}`,
+                        scrub: true, 
+                        markers: true,
+                        invalidateOnRefresh: true,
+                    }
+                });
+            }
+
+            initHorizontalScroll();
+        })
