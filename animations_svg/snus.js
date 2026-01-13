91ca8bf1ad33bfb87092520cb1bfe5989eec08ea
elsp02 <elena.spitzer@hs-ausgburg.de>
2026-01-13 10:35:47 +0100

    snus animation angepasst

--- animations_svg/snus.js
+++ animations_svg/snus.js
@@ -1,24 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-gsap.fromTo(
-  "#flyImg",
-  { x: "-150px", rotation: 0 },
-  {
-    x: "200vw",
-    xPercent: -100,
-    rotation: 520,
-    duration: 3,
-    ease: "none",
-    transformOrigin: "50% 65%"
-    /*scrollTrigger: {
-      trigger: "#snusContainer",*/
-  }
-);
-
-  // Wabbeln (kleine vertikale Bewegung)
-  gsap.to("#flyImg", {
-    y: 6,                 // Stärke des Wabbelns
-    duration: 0.35,
-    ease: "sine.inOut",
-    repeat: -1,
-    yoyo: true
-  });
+  gsap.fromTo(
+    "#flyImg",
+    { x: "-150px", y: -220, rotation: 0 },
+    {
+      x: "200vw",
+      y: 300,
+      xPercent: -100,
+      rotation: 520,
+      duration: 10,
+      ease: "expoScale",
+      transformOrigin: "10% 65%",
+      scrollTrigger: {
+        trigger: ".container",
+        start: "bottom top",
+        // optional zum Testen:
+        // markers: true
+      }
+    }
+  );
+});
